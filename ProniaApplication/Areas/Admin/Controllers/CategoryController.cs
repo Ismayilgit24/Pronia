3795d8503616271d0ad0fef10b7ce5cb9d2deb0d
Ismayilgit24 <ismayilibrahinzad9@gmail.com>
2024-11-24 19:58:49 +0400

    initial commit

---
+++ ProniaApplication/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProniaApplication.DAL;
+using ProniaApplication.Models;
+
+namespace ProniaApplication.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly AppDBContext _context;
+
+        public CategoryController(AppDBContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<Category> categories = await _context.Categories.Include(c=>c.Products).ToListAsync();
+
+            return View(categories);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Category category)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            bool result = await _context.Categories.AnyAsync(c => c.Name.Trim() == category.Name.Trim());
+            if(result)
+            {
+                ModelState.AddModelError("Name", $"{category.Name} is already exist");
+                return View();
+            }
+
+            category.CreatedAt = DateTime.Now;
+
+            await _context.Categories.AddAsync(category);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+           
+        }
+    }
+}
